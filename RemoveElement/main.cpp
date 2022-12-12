2e6df1a92b74e25955c0f3e92c89afde02bc4d16
Henrique Oliveira <henrique.gomes_de_oliveira@syngenta.com>
2022-12-12 08:45:15 -0300

    removeElement

--- RemoveElement/main.cpp
+++ RemoveElement/main.cpp
@@ -2,14 +2,17 @@
 #include <vector>
 
 int removeElement(std::vector<int>& nums, int val) {
-    int i = 0, j = 0;
-    while(j != nums.size())
-        if(nums[j] == val) j++;
-        else nums[i++] = nums[j++];
+    int i = 0;
+    for (int num : nums){
+        if(num != val){
+            nums[i++] = num;
+        }
+    }
     return i;
 }
 
-int main(){
+int main() {
+    // std::vector<int> v = {3,2,2,3}; //val = 3
     std::vector<int> v = {0,1,2,2,3,0,4,2};
     std::cout << removeElement(v, 2) << std::endl;
 }
